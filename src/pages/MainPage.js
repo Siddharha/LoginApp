0e669d3a9f1d570ede79526a21f2d4674c53d249
Siddhartha <siddhartha.ee.13@gmail.com>
2018-11-27 12:29:48 +0530

    asdfsdf

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet,Text , View } from 'react-native';
+import { Card, ListItem  } from 'react-native-elements'
 export default class MainPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={dataList:[{
+      name: 'Amy Farha',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Vice President'
+    },
+    {
+      name: 'Chris Jackson',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Vice Chairman'
+    },
+    {
+      name: 'Amy Farha',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Vice President'
+    },
+    {
+      name: 'Chris Jackson',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Vice Chairman'
+    },
+    ]}
+  }
+
   render() {
     return (
       <View style={styles.container}>
-       
+{
+  this.state.dataList.map((l, i) => (
+
+    <Card  key={i} >
+<ListItem
+      
+      leftAvatar={{ source: { uri: l.avatar_url } }}
+      title={l.name}
+      subtitle={l.subtitle}
+    />
+    </Card>
+ 
+   
+  ))
+}
       </View>
     );
   }
@@ -16,44 +55,11 @@ export default class MainPage extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-   textStyle: {
-    color:'#00bcd4',
-    fontWeight:"bold",
-    fontSize: 30,
-    marginBottom: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  netTextStyle:{
-    color:'#000',
-    fontWeight:"bold",
-    fontSize: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputStyle:{
-    height: 40, 
-    margin: 5,
-    padding:10,
-    borderColor: 'gray', 
-    borderWidth: 1,
-    width:'90%',
-  
-  },
-  button:{
-    margin: 10,
-    padding:15,
-    backgroundColor:'#000'
-  },
-  btn_txt:{
-    color: '#fff',
-    fontWeight:"bold",
+   },
+  item: {
+    padding: 10,
     fontSize: 18,
+    height: 44,
   }
 
 });
